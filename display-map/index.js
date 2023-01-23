d6665c0bdf5fd2dedf52df9fbb775a405f767e57
brayan ramirez <brayanramirez4546@gmail.com>
2023-01-23 08:41:37 -0500

    cod 6

--- display-map/index.js
+++ display-map/index.js
@@ -8,9 +8,9 @@
     esriConfig.apiKey = "AAPK3a46155e72e141f7a1e526779e26ee9eAjXhDfndw1NljGMOgVuF26eFX3-eeUXZZOyG1ndpeWqpSuegyvOx67wvbav2qGrx";
   // tipo de mapa 
     const map = new Map({
-      basemap: "topo-vector" // Basemap layer service
+      basemap: "topo-vector" // Basemap layer service // hay varias bases
     });
-// visualizamos el mapa y ajustamos las cordenadas
+// visualizamos el mapa y ajustamos las cordenadas tanbien s epuede hacer en 3d si usamos el  new SceneView
     const view = new MapView({
       map: map,
       center:[-118.80500,34.02700], // Longitude, latitude
